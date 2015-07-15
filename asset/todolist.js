a8830c27474d0252178736a8071ae9253f4a1071
Smith <smithoo@nhnnext.org>
2015-07-16 00:36:54 +0900

    week1_complete

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,33 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-	document.getElementById("new-todo").addEventListener("keydown", addTodo);
+$("document").ready(function() {
+	$("#new-todo").on("keydown", addTodo);
 });
 
 function addTodo(event) {
 	var ENTER_KEYCODE = 13;
-	var newTodo = document.getElementById("new-todo");
+	var newTodo = $("#new-todo").get(0);
 	if (event.keyCode === ENTER_KEYCODE) {
 		var todo = makeTodoItem(newTodo.value);
-		document.getElementById("todo-list").appendChild(todo);
 		newTodo.value = "";
 	}
 }
 
 function makeTodoItem(todo) {
-	var newItem = document.createElement("li");
-	var div = document.createElement("div");
-	div.className = "view";
-	var input = document.createElement("input");
-	input.className = "toggle";
-	input.setAttribute("type", "checkbox");
-	var label = document.createElement("label");
-	label.innerText = todo;
-	var button = document.createElement("button");
-	button.className = "destroy";
-
-	div.appendChild(input);
-	div.appendChild(label);
-	div.appendChild(button);
-	newItem.appendChild(div);
-
-	return newItem;
+	var template = $('#template').html();
+	Mustache.parse(template);
+	var rendered = Mustache.render(template, {title: todo});
+	$('#todo-list').append(rendered);
 }
