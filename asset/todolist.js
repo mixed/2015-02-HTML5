ee643b752709809731bbcfd35e6edb92b86c57bf
Smith <smithoo@nhnnext.org>
2015-07-21 08:37:52 +0900

    week2

--- asset/todolist.js
+++ asset/todolist.js
@@ -6,14 +6,45 @@ function addTodo(event) {
 	var ENTER_KEYCODE = 13;
 	var newTodo = $("#new-todo").get(0);
 	if (event.keyCode === ENTER_KEYCODE) {
-		var todo = makeTodoItem(newTodo.value);
+		makeTodoItem(newTodo.value);
 		newTodo.value = "";
 	}
-}
+};
 
 function makeTodoItem(todo) {
 	var template = $('#template').html();
 	Mustache.parse(template);
 	var rendered = Mustache.render(template, {title: todo});
 	$('#todo-list').append(rendered);
-}
+	var todo = $('#todo-list li:last');
+	todo.on("click", controlTodo);
+};
+
+function controlTodo(event) {
+	var eTarget = event.target;
+	if (eTarget.tagName === "INPUT") {
+		completedTodo(event);
+	}
+	else if (eTarget.tagName === "BUTTON") {
+		removeTodo(event);
+	}
+};
+
+function completedTodo(event) {
+	var input = event.target;
+	var li = event.currentTarget;
+	if (input.checked) {
+		li.classList.add("completed");
+	}
+	else {
+		li.classList.remove("completed");
+	}
+};
+
+function removeTodo(event) {
+	var li = event.currentTarget;
+	li.classList.add("deleting");
+	li.addEventListener("transitionend", function() {
+		this.remove();
+	});
+};
