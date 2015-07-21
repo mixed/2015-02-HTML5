8eb8dd474c531628fd94c240da301804a3a338d5
Smith <smithoo@nhnnext.org>
2015-07-21 17:28:40 +0900

    week2_complete

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,50 +1,77 @@
+/*var xhr = new XMLHttpRequest();
+xhr.open("GET", "", false);
+xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+xhr.addEventListener()
+xhr.send(null);
+result = xhr.responseText;
+result = JSON.parse(xhr);*/
+
+var TODOSync = {
+	get : function() {
+
+	},
+	add : function() {
+
+	},
+	completed : function() {
+
+	},
+	remove : function() {
+
+	}
+};
+
+var TODO = {
+	ENTER_KEYCODE : 13,
+	init : function() {
+		$("#new-todo").on("keydown", this.build.bind(this));
+		$('#todo-list').on("click",this.controller.bind(this));
+	},
+	build : function(event) {
+		var newTodo = $("#new-todo");
+		if (event.keyCode === this.ENTER_KEYCODE) {
+			this.makeItem(newTodo.val());
+			newTodo.val("");
+		}
+	},
+	makeItem : function(todo) {
+		var template = $('#template').html();
+		Mustache.parse(template);
+		var rendered = Mustache.render(template, {title: todo});
+		$('#todo-list').append(rendered);
+
+		//질문!!!
+		//CSS Update라는 개념이 와닿지 않습니다.
+		$('#todo-list li:last').css('opacity');
+		$('#todo-list li:last').removeClass("appending");
+	},
+	controller : function(event) {
+		var eTarget = event.target;
+		if (eTarget.tagName === "INPUT") {
+			this.completed(eTarget);
+		}
+		else if (eTarget.tagName === "BUTTON") {
+			this.removeItem(eTarget);
+		}
+	},
+	completed : function(eTarget) {
+		var li = $(eTarget).closest("li");
+		if (eTarget.checked) {
+			li.addClass("completed");
+		}
+		else {
+			li.removeClass("completed");
+		}
+	},
+	removeItem : function(eTarget) {
+		var li = $(eTarget).closest("li");
+		li.addClass("deleting");
+		li.on("transitionend", function() {
+			li.remove();
+		});
+	}
+};
+
 $("document").ready(function() {
-	$("#new-todo").on("keydown", addTodo);
+	TODO.init();
 });
-
-function addTodo(event) {
-	var ENTER_KEYCODE = 13;
-	var newTodo = $("#new-todo").get(0);
-	if (event.keyCode === ENTER_KEYCODE) {
-		makeTodoItem(newTodo.value);
-		newTodo.value = "";
-	}
-};
-
-function makeTodoItem(todo) {
-	var template = $('#template').html();
-	Mustache.parse(template);
-	var rendered = Mustache.render(template, {title: todo});
-	$('#todo-list').append(rendered);
-	var todo = $('#todo-list li:last');
-	todo.on("click", controlTodo);
-};
-
-function controlTodo(event) {
-	var eTarget = event.target;
-	if (eTarget.tagName === "INPUT") {
-		completedTodo(event);
-	}
-	else if (eTarget.tagName === "BUTTON") {
-		removeTodo(event);
-	}
-};
-
-function completedTodo(event) {
-	var input = event.target;
-	var li = event.currentTarget;
-	if (input.checked) {
-		li.classList.add("completed");
-	}
-	else {
-		li.classList.remove("completed");
-	}
-};
-
-function removeTodo(event) {
-	var li = event.currentTarget;
-	li.classList.add("deleting");
-	li.addEventListener("transitionend", function() {
-		this.remove();
-	});
-};
